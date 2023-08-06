f6470ec1723884b6cccaec303c0de42a80c52812
daffydbest7 <daffydbest@gmail.com>
2023-08-06 15:00:32 +0100

    cean up

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../store/actions/user';
 import {Link, useNavigate } from 'react-router-dom';
 import {BiSolidMoon} from "react-icons/bi";
-import {PiSunLight} from "react-icons/pi";
+//import {PiSunLight} from "react-icons/pi";
 
 
 
--- frontend/src/pages/articleDetail/ArticleDetailPage.jsx
+++ frontend/src/pages/articleDetail/ArticleDetailPage.jsx
@@ -12,14 +12,14 @@ import { getAllPosts, getSinglePost } from "../../services/index/posts";
 import ArticleDetailSkeleton from "./components/ArticleDetailSkeleton";
 import ErrorMessage from "../../components/ErrorMessage";
 import { useSelector } from "react-redux";
-import parseJsonToHtml from "../../utils/parseJsonToHtml";
-import Editor from "../../components/editor/Editor";
+//import parseJsonToHtml from "../../utils/parseJsonToHtml";
+//import Editor from "../../components/editor/Editor";
 
 const ArticleDetailPage = () => {
   const { slug } = useParams();
   const userState = useSelector((state) => state.user);
   const [breadCrumbsData, setbreadCrumbsData] = useState([]);
-  const [body, setBody] = useState(null);
+  //const [body, setBody] = useState(null);
 
   const { data, isLoading, isError } = useQuery({
     queryFn: () => getSinglePost({ slug }),
